8e8adeb8636ebe38e39cfcd214cc82b40fc00102
Muhammadkhuja <muhammadxuja9@gmail.com>
2025-05-28 11:17:17 +0500

    candy Start

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ envFilePath: ".env", isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: config.get<"postgres">("DB_CONNECTION"),
+        host: config.get<string>("DB_HOST"),
+        username: config.get<string>("DB_USERNAME"),
+        password: config.get<string>("DB_PASSWORD"),
+        port: config.get<number>("DB_PORT"),
+        database: config.get<string>("DB_NAME"),
+        entities: [__dirname + "dis/**/*.entity{.ts,.js}"],
+        synchronize: true,
+        autoLoadEntities: true,
+        logging: false,
+      }),
+    }),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
